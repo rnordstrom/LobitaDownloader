1612baba65dbe94d245044e71daa3fdb6c4afadb
rnordstrom <rikard.nordstrom94@gmail.com>
2021-05-09 16:04:43 +0200

    Improve console logging

--- LobitaDownloader/LobitaDownloader/Index/DbIndexPersistence.cs
+++ LobitaDownloader/LobitaDownloader/Index/DbIndexPersistence.cs
@@ -44,7 +44,9 @@ namespace LobitaDownloader
         {
             try
             {
-                Console.WriteLine("Cleaning database...");
+                string output = "Cleaning database...";
+
+                PrintUtils.PrintRow(output, 0, 0);
 
                 conn.Open();
                 
@@ -65,7 +67,6 @@ namespace LobitaDownloader
                 string checkExists = $"SELECT {idColumn} FROM {tableName} WHERE {idColumn} = {currentId}";
                 string deleteBatch = $"DELETE FROM {tableName} LIMIT {BatchQueryLimit}";
                 string resetInc = $"ALTER TABLE {tableName} AUTO_INCREMENT = 1";
-                string output;
 
                 MySqlCommand existsCmd = new MySqlCommand(checkExists, conn);
                 MySqlCommand deleteCmd = new MySqlCommand(deleteBatch, conn);
@@ -84,7 +85,7 @@ namespace LobitaDownloader
                     existsCmd = new MySqlCommand(checkExists, conn);
 
                     rdr = existsCmd.ExecuteReader();
-                    output = $"Deleted {deleted} rows.";
+                    output = $"Deleted {deleted} rows from table '{tableName}'.";
 
                     PrintUtils.PrintRow(output, 0, 0);
                 }
